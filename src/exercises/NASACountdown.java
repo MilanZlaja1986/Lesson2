bb1bb4c6a205140fdf884567a3f008df3b524fba
MilanZlaja1986 <milan.zlatkovic@hotmail.rs>
2019-04-22 10:21:29 +0200

    Second change

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -1,11 +1,28 @@
 package exercises;
 
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  * Count down a rocket launch!
  * 
- * Print the numbers from 10 to 1, then print “blastoff”. 
+ * Print the numbers from 10 to 1, then print â€œblastoffâ€�. 
  */
 
-public class NASACountdown {
 
+public class NASACountdown {
+	
+	public static void main(String[] args){
+		
+		NASACountdown rocket = new NASACountdown();
+		
+		for (int i = 10; i >= 0; i--) {
+
+			System.out.println(i);
+			
+			if (i == 1) {
+				System.out.println("Blastoff!");
+				break;
+			}
+		}
+	}
 }
